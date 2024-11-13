3bf2d3f29991ddfcc2201b0bb69eac9a70440661
Tearever <trevor.wright17@yahoo.com>
2024-11-12 21:49:31 -0600

    Added BackgroundScaler Script
    Added new features:
        - Dynamically adjust the scale of the background gameobject to fit the screen based on the resolution.

---
+++ Assets/Script/BackgroundScaler.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    private void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        // Get screen size
+        float screenHeight = Camera.main.orthographicSize * 2.0f;
+        float screenWidth = screenHeight / Screen.height * Screen.width;
+
+        // Get sprite size
+        Vector2 spriteSize = sr.sprite.bounds.size;
+        
+        // Scale sprite to fit the screen size
+        transform.localScale = new Vector3(screenWidth / spriteSize.x, screenHeight / spriteSize.y, 1);
+    }
+}
